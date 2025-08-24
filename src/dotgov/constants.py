7d2015fa6b67f71e1eef30da967a4a98ec6ebbcb
Germán Muñoz <46906587+munozbravo@users.noreply.github.com>
2025-08-24 15:09:15 -0500

    Removed dataset ids.
    Only keep locations.

--- src/dotgov/constants.py
+++ src/dotgov/constants.py
@@ -5,12 +5,3 @@ COLOMBIA = "www.datos.gov.co"
 SEATTLE = "data.seattle.gov"
 
 CHICAGO = "data.cityofchicago.org"
-
-
-## Datasets
-
-COLOMBIA_PROCUREMENT = "p6dx-8zbt"
-
-SEATTLE_911 = "kzjm-xkqj"
-
-CHICAGO_CRIMES = "ijzp-q8t2"
